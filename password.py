6a64d94bd8c1bb9283c28f91d79b592de49d153b
daniyalkarwnd2004 <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-03-08 23:21:00 +0330

    Update password.py

--- password.py
+++ password.py
@@ -1,5 +1,9 @@
+# add random
+
 import random
 import string
+
+# add lowercase and uppercase and number
 
 lowe_case = string.ascii_lowercase
 upper_case = string.ascii_uppercase
@@ -7,7 +11,13 @@ number = "0123456789"
 symbol = "~!@#$%^&*(){}"
 password_all = lowe_case + upper_case + number + symbol
 
+
+# I created a loop and displayed the items for the user to select
+
 while True:
+
+# Create a condition and get a number from the user for the number of digits of the code
+    
     print("please select : \n1)create a password\n2)exit")
     select = input("please select : ")
     if select == "1":
