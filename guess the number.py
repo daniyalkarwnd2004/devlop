caf1291ba24d35810218e3bf814c940e7b6e0a9a
daniyal karwnd <karwnddaniyal16@gmail.com>
2024-03-08 13:42:38 +0330

    add if and elif and else

--- guess the number.py
+++ guess the number.py
@@ -3,13 +3,13 @@ number = random.randint(1, 10)
 digit = int(input("Enter Number : "))
 #
 while number != digit:
-#     print()
-#     if digit < number :
-#         print("digit is low")
-#         digit = int(input("Enter Number : "))
-#     elif digit > number :
-#         print("digit is high")
-#         digit = int(input("Enter Number : "))
-#     else:
-#         print("you guessed right !")
-#         break
+    print()
+    if digit < number :
+        print("digit is low")
+        digit = int(input("Enter Number : "))
+    elif digit > number :
+        print("digit is high")
+        digit = int(input("Enter Number : "))
+    else:
+        print("you guessed right !")
+        break
