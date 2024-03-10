8655535d0f02b42ebd63184783019a305591f40b
daniyalkarwnd2004 <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-03-10 13:31:16 +0330

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,19 @@
+
+# we take the height and weight to do the calculation
+def bmi(height, weight):
+    bmi_project = (weight / (height ** 2))
+    if bmi_project < 18.5:
+        print("underweight")
+    elif bmi_project >= 18.5 and bmi_project <= 24.9:
+        print("healthy weight")
+    elif bmi_project  >= 25.0 and bmi_project <= 29.9:
+        print("obesity => class one")
+    elif bmi_project >= 30.0 and bmi_project <= 34.9:
+        print("obesity => class two")
+    elif bmi_project >= 35.0 and bmi_project <= 39.9:
+        print("obesity => class three")
+
+
+height1 = float(input("height : "))
+weight1 = float(input("weight : "))
+bmi(height1, weight1)
