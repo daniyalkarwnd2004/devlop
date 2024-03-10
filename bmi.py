8201ff261ce2897f152e3fdcfd458dc1ecf0e656
daniyalkarwnd2004 <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-03-10 13:34:50 +0330

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,7 +1,13 @@
 
 # we take the height and weight to do the calculation
 def bmi(height, weight):
+
+    # We receive the height and weight from the user and calculate them with the following formula
+    
     bmi_project = (weight / (height ** 2))
+
+   Using the conditions, we calculate the body mass index equation # 
+    
     if bmi_project < 18.5:
         print("underweight")
     elif bmi_project >= 18.5 and bmi_project <= 24.9:
