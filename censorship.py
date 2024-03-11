adf06ee968f50bb2e2e9903cc8933851c197428e
daniyalkarwnd2004 <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-03-11 12:31:11 +0330

    censorship.py

--- censorship.py
+++ censorship.py
@@ -1,3 +1,6 @@
+# This file does the work of censorship
+Finds the entry and compares it with the elements in the list. If these elements are the same as the elements in the list, it prints a star
+
 def censorship(words):
     print("****-------censorship-------****")
     w = None
