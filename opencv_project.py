169a0296007e2c32e5a5cb8411667edc525d5fbb
I am Daniel <162690900+daniyalkarwnd2004@users.noreply.github.com>
2024-10-19 10:58:58 +0330

    Add files via upload

--- opencv_project.py
+++ opencv_project.py
@@ -1,23 +1,11 @@
 import cv2 as cv
 import numpy as np
-# Image processing program
-# This program checks the packages and confirms them if they are square and undamaged
+from matplotlib import pyplot as plt
 
-
-def open_cv():
-    img = cv.imread("regtangle.jpg", 0)
-    img2 = cv.cornerHarris(img, 3, 9, 0.04)
-    corner = cv.dilate(img2, None)
-    img[corner > 0.01 * corner.max()] = 255
-    threshold = 0.01 * corner.max()
-    corner_points = np.argwhere(corner > threshold)
-    num_corners = len(corner_points)
-    if num_corners == 136:
-        print("yes closed is True")
-    else:
-        print("yes closed is False")
-    cv.imshow("img", img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-open_cv()
+img1 = cv.imread("xray2.jpg", 0)
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+cl1 = clahe.apply(img1)
+histogram = cv.calcHist([cl1], [0], None, [256], [0, 256])
+plt.subplot(121), plt.imshow(cl1, "gray")
+plt.subplot(122), plt.plot(histogram)
+plt.show()
